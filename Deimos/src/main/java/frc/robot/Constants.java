c11b2fd3e9107e7d4751a0accf40e12c9a3d870a
Arhum Mudassir <97483940+IsabelleFleur@users.noreply.github.com>
2024-01-25 19:29:01 -0500

    Changed constants

--- Deimos/src/main/java/frc/robot/Constants.java
+++ Deimos/src/main/java/frc/robot/Constants.java
@@ -31,7 +31,7 @@ public final class Constants {
         public static final double kPX = 1.25;
         public static final double kPY = 1.25;
 
-        public static final double kAutoRotateEpsilon = 1.0;
+        public static final double kAutoRotateEpsilon = 3.0;
         public static final double kLinearDriveTranslationEpsilon = 0.04;//.05
         public static final double kLinearDriveRotationEpsilon = 2.0;
         
