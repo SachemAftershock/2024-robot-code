c8e8103e1aa7fbacbf295cb39167be46e3a852c0
arhummudassir <arhummudassir@gmail.com>
2024-02-12 16:49:26 -0500

    Fixed intake to continously run a stateless PID to control motion

--- Deimos/src/main/java/frc/robot/RobotContainer.java
+++ Deimos/src/main/java/frc/robot/RobotContainer.java
@@ -63,7 +63,6 @@ public class RobotContainer {
             () -> -modifyAxis(-mControllerPrimary.getLeftX()) * DriveConstants.kMaxVelocityMetersPerSecond * 0.7,//() -> -modifyAxis(mControllerPrimary.getLeftX()) * DriveConstants.kMaxVelocityMetersPerSecond * 0.7,
 
             () ->-modifyAxis(mControllerSecondary.getTwist()) * DriveConstants.kMaxAngularVelocityRadiansPerSecond * 0.3//() -> -modifyAxis(mControllerSecondary.getTwist()) * DriveConstants.kMaxAngularVelocityRadiansPerSecond * 0.3
-
     ));
     // mIntakeSubsystem.setDefaultCommand(new ManualIntakeArm(
     //         mIntakeSubsystem,
@@ -94,13 +93,13 @@ public class RobotContainer {
     Trigger IntakeArmIngestTrigger = new Trigger(() -> mControllerPrimary.getAButton());
 
     IntakeArmIngestTrigger.onTrue(new InstantCommand(() -> { 
-      mIntakeSubsystem.setDesiredIntakeState(IntakeState.eRetracted);
+      mIntakeSubsystem.setIntakePosition(IntakeState.eRetracted.getDesiredPosition());
     }));
 
     Trigger negIntakeArmIngestTrigger = new Trigger(() -> mControllerPrimary.getYButton());
 
     negIntakeArmIngestTrigger.onTrue(new InstantCommand(() -> { 
-      mIntakeSubsystem.setDesiredIntakeState(IntakeState.eDeployed);
+      mIntakeSubsystem.setIntakePosition(IntakeState.eDeployed.getDesiredPosition());
     }));
 
 
--- Deimos/src/main/java/frc/robot/enums/IntakeState.java
+++ Deimos/src/main/java/frc/robot/enums/IntakeState.java
@@ -8,8 +8,15 @@ public enum IntakeState {
     eRetracted(0.0),//TODO: change to eRetracted
     // FIXME. This constant concerns SAFETY, so it needs to be written by a person with
     // knowledge pertaining to what this "safety" even is
-    eSafeShooterMovement(0.0); //FIXME might be wrong 
+    eSafeShooterMovement(0.0), //FIXME might be wrong 
     //Not totally out but far out enought to be safe to move the shooter without fear of getting caught on intake
+
+    //Describes the intake state between being retracted and deployed, since we are calibrated 
+    //we can assume we are between states
+    eInMotion(0.0),
+    
+    //Intake is not calibrated or something went wrong
+    eUnknown(0.0);
     
     private double mIntakeEncoderPosition;
 
